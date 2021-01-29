87608f1605d014fca1dc8f3614e9a97526845278
tiagoribas2021 <tiagoribeiroa2o@gmail.com>
2021-01-29 17:58:50 -0300

    class Posicao

--- src/aplicacao/Program.java
+++ src/aplicacao/Program.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import jogoTabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Bom dia Mundo");
+		Posicao posicao = new Posicao(1, 3);
+		
+		System.out.println(posicao);
 
 	}
 
---
+++ src/jogoTabuleiro/Posicao.java
@@ -0,0 +1,32 @@
+package jogoTabuleiro;
+
+public class Posicao {
+
+	private int linha;
+	private int coluna;
+	
+	public Posicao(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
+	public String toString() {		
+		return linha +", "+coluna;	
+	}
+}
