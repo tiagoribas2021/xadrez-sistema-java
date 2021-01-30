483881208ff2f5d558611dd79f0f3c7611dfe23d
tiagoribas2021 <tiagoribeiroa2o@gmail.com>
2021-01-29 22:54:44 -0300

    new repository

--- src/aplicacao/Program.java
+++ src/aplicacao/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Posicao posicao = new Posicao(1, 3);
+		Posicao posicao = new Posicao(3, 5);
 		
 		System.out.println(posicao);
 
